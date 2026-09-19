88b038b4a0633e9dd6a176a1714f59d2ddfef59f
AnnkoATAMA <annkoatama0903@gmail.com>
2026-09-20 06:03:15 +0900

    Refresh installed PWA after deployments

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
-if ('serviceWorker' in navigator && import.meta.env.PROD) window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => {}))
+if (import.meta.env.PROD) registerSW({ immediate: true })
